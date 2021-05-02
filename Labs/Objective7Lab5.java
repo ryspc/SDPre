ae3db0eac1e2a9294f39f03f013d1829bba572dc
Ray S <rayspace@protonmail.com>
2021-05-02 04:11:00 -0600

    complete objective7lab5

--- Labs/Objective7Lab5.java
+++ Labs/Objective7Lab5.java
@@ -15,15 +15,14 @@ public class Objective7Lab5 {
 
       selection = scanner.nextInt();
 
-      if (selection == 1) {
+      if(selection == 1) {
       System.out.println("Hello Human");
       }
-      else if (selection == 2) {
+      else if(selection == 2) {
       System.out.println("Apples, Bananas, Coconuts");
       }
-      else if (selection == 3) {
+      else if(selection == 3) {
       System.out.println("Goodbye");
-      break;
       }
       /*
       Your solution goes here

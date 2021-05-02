3c1c0c5d8895665e0917b1da4d746777a2211440
Ray S <rayspace@protonmail.com>
2021-05-02 04:17:45 -0600

    complete Objective7Lab5

--- Labs/Objective9Lab3.java
+++ Labs/Objective9Lab3.java
@@ -1,21 +1,15 @@
 import java.util.Scanner;
 
-public class Objective7Lab5 {
+public class Objective9Lab3 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int selection;
 
-    while (true) {
-      System.out.println("_____Menu_____");
-      System.out.println("1: Say Hello");
-      System.out.println("2: List My favorite foods");
-      System.out.println("3: Exit");
-      System.out.println();
-
+    while(true){
+      printMenu();
       selection = scanner.nextInt();
-
       if(selection == 1) {
-        System.out.println("Hello World");
+        System.out.println("Hello Human");
       }
       else if(selection == 2) {
         System.out.println("Apples, Bananas, Coconuts");
@@ -24,7 +18,20 @@ public class Objective7Lab5 {
         System.out.println("Goodbye");
         break;
       }
+      /*
+      Your solution goes here
+      */
     }
+
     scanner.close();
+
+  }
+
+  public static void printMenu() {
+    System.out.println("_____Menu_____");
+    System.out.println("1: Say Hello");
+    System.out.println("2: List My favorite foods");
+    System.out.println("3: Exit");
+    System.out.println();
   }
 }

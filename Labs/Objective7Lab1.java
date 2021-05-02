629e2d7ff67f29df85fb38ccb70d976eeae7f0d8
Ray S <rayspace@protonmail.com>
2021-05-02 01:55:50 -0600

    complete objective7lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,15 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    int counter = 10;
+
+    while(counter > 0) {
+      System.out.println(counter);
+
+      /*
+      Your solution goes here
+      */
+
+      counter = counter - 1; //this makes we count down
+    }
+  }
+}

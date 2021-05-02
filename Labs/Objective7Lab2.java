9efd7a22d38b894fdb8f02a5aa8051c594fc42b0
Ray S <rayspace@protonmail.com>
2021-05-02 02:00:00 -0600

    complete objective7lab2

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,14 @@
+public class Objective7Lab2 {
+  public static void main(String[] args) {
+  int counter = 0;
+
+    while(counter <= 20) {
+
+      System.out.println(counter);
+      counter = counter +1;
+      /*
+      Your solution goes here
+      */
+    }
+  }
+}

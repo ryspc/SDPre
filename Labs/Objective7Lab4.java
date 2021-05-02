51d428e620648526e3f8ee95bed82622726ef8dd
Ray S <rayspace@protonmail.com>
2021-05-02 02:33:30 -0600

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,13 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int count = 1;
+    int sum = 1;
+
+    while(count < 20) {
+      count = count + 1;
+      sum = count + sum;
+
+    }
+    System.out.println(sum);
+  }
+}

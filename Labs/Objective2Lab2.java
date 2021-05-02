82fb5ef21f95c99acaf63119deeb8dc82f9a5e94
Ray S <rayspace@protonmail.com>
2021-05-01 18:02:38 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Ugur Acar";
+    int age = 32;
+    String state = "Colorado";
+    System.out.println("Name: "+ name + " Age: " + age + " State: " + state);
+  }
+}

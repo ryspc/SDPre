e511e117c893da91425857617632f07b60cd47c1
Ray S <rayspace@protonmail.com>
2021-05-01 19:59:09 -0600

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,10 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1, side2, hypotenuse;
+    side1 = 10;
+    side2 = 8;
+    hypotenuse = (side1 * side1) + (side2 * side2);
+    System.out.println("The hypotenuse of a triange with sides " + side1 + " and " + side2 + " is " + Math.sqrt(hypotenuse));
+
+  }
+}
